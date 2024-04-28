7a30e23fde15be9d0de2ffbb2b89cdb903e7efc4
Julieta Collado <153026599+JulietaCCollado@users.noreply.github.com>
2024-04-28 11:06:46 +0200

    Update README.py

--- README.py
+++ README.py
@@ -21,8 +21,3 @@ class Information:
           return ["Flask", "Cypress", "Playwright", "Django"]
      
   
-
-<!---
-JulietaCCollado/JulietaCCollado is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
