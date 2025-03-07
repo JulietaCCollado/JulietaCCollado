946ed3408d4cb6332203814a969c58bfdc69bbdd
Julieta Collado <153026599+JulietaCCollado@users.noreply.github.com>
2025-03-07 18:17:09 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,4 @@
-from SoftwareDeveloper import JulietaCollado
+from SoftwareDeveloper import UserProfile
 
 class Information:
    def __init__(self):
@@ -6,18 +6,20 @@ class Information:
       self.age = '26 years old'
       self.from = 'Buenos Aires, Argentina'
       self.residence = 'Copenhagen, Denmark'
-      self.hobbies = 'Travel, coding, go for walks'
+      self.hobbies = 'Travel, coding, cooking, strolls'
       self.contact = 'julietacollado98@hotmail.com'
-      self.linkedIn = 'Julieta Marina Colberg Collado'
+      self.linkedIn = 'Julieta Marina Collado'
       self.spokenLanguages = 'English, Spanish, Italian'
       
 
-  def skills(self, languages, databases, frameworks):
+  def skills(self, languages, tools, frameworks,ai_skills):
       if languages:
          return ["Python", "JavaScript", "HTML", "CSS"]
-      elif databases:
-         return ["SQLite", "MySQL"]
+      elif tools:
+         return ["Git", "Docker", "VSCode", "PyCharm"]
       elif frameworks:
-          return ["Flask", "Cypress", "Playwright", "Django"]
+          return ["PyTorch", "Flask", "Django", "TensorFlow"]
+      elif ai_skills:
+         return [ "Machine Learning", "Deep Learning", "Neural Networks", "Computer Vision", "Natural Language Processing", "PyTorch", "TensorFlow", "keras", "Scikit-learn"] 
      
   
